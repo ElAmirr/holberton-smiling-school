c0064cf25a70cad19331279ffc215ef219a6715e
ElAmirr <othmaniamir41@gmail.com>
2022-01-31 18:55:57 +0100

    push

---
+++ index.js
@@ -0,0 +1,9 @@
+// small divice nav
+function openNav() {
+  document.getElementById("myNav").style.display = "block";
+  console.log('hello');
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.display = "none";
+}
